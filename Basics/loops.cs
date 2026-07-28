66552bc42df2ae7546c534fcead825587a01188c
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-28 21:25:04 +0530

    Types of loops Solved a Question.

--- Basics/loops.cs
+++ Basics/loops.cs
@@ -1,6 +1,28 @@
+// While
 int counter = 0;
 while (counter < 10)
 {
     Console.WriteLine($"Hey I am called {counter} time.");
     counter++;
 }
+
+// Do while 
+int num = 0;
+do
+{
+    Console.WriteLine($"Hey I am called {num} time.");
+    num++;
+}
+while (num < 10);
+
+// Find the sum of all integers 1 through 20 that are divisible by 3
+int sum = 0;
+for (int i = 1; i < 21; i++)
+{
+    if (i % 3 == 0)
+    {
+        sum += i;
+    }
+
+}
+Console.WriteLine($"Sum is {sum}");
