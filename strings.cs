2e3dd642ce5504c8b3bd8350721222477e1073d1
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-28 15:15:02 +0530

    Basics of Strings

---
+++ strings.cs
@@ -0,0 +1,6 @@
+string friend1 = "Aman";
+string friend2 = "Anurag";
+// Console.WriteLine(hello + " " + aFriend);
+
+Console.WriteLine($"Hello {friend2.Length} Myself {friend1}"); //C# does not provide backticks conditioning like Javascript and TypeScript.
+
