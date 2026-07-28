caa6636a3d8afa710953746904818794a55aa90e
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-28 18:57:07 +0530

    Basics of numbers

---
+++ Basics/numbers.cs
@@ -0,0 +1,14 @@
+int a = 30;
+int b = 20;
+int c = a + b;
+Console.WriteLine($"Addition of {a} and {b} is : {c}");
+
+// long a1 = 100000000000000000;
+// long b1 = 200000000000000000;
+// long c1 = a1 + b1;   // Int type only supports upto 32 bit so we will use long for this..
+// Console.WriteLine($"Addition of {a1} and {b1} is : {c1}");
+
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
