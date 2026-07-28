293c2398636b97fa2b1ae7b3d51d3a45bd752366
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-28 19:06:23 +0530

    Types of numbers and their deffault type

--- Basics/numbers.cs
+++ Basics/numbers.cs
@@ -1,7 +1,7 @@
-int a = 30;
-int b = 20;
-int c = a + b;
-Console.WriteLine($"Addition of {a} and {b} is : {c}");
+// int a = 30;
+// int b = 20;
+// int c = a + b;
+// Console.WriteLine($"Addition of {a} and {b} is : {c}");
 
 // long a1 = 100000000000000000;
 // long b1 = 200000000000000000;
@@ -9,11 +9,23 @@ Console.WriteLine($"Addition of {a} and {b} is : {c}");
 // Console.WriteLine($"Addition of {a1} and {b1} is : {c1}");
 
 
+// BY DEFAULT DECIMAL NUMBERS - FLOAT AND WHOLE NUMBERS - INT.
+
 int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}");
 
+// OVERFLOW
 int what = max + 3; //Overflow Wrapping
 int what2 = min - 3; //Underflow Wrapping
 Console.WriteLine($"An example of overflow: {what}");
 Console.WriteLine($"An example of overflow: {what2}");
+
+
+
+// Double type
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
