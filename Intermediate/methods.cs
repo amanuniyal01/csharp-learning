f971f02f78b7dc91c618d6ad5bb373aa6d983104
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-29 08:23:32 +0530

    Basics of methods

---
+++ Intermediate/methods.cs
@@ -0,0 +1,15 @@
+MyMethod();
+Console.WriteLine(Add(2, 3));
+
+// METHOD..
+static void MyMethod()
+{
+    Console.WriteLine("Hello, I am a method");
+}
+
+// ADDITION METHOD
+static int Add(int a, int b)
+{
+    return a + b;
+
+}
