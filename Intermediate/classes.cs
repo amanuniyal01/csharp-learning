5f3108e86807f5c1f5fa7190ba4d94bc6c428644
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-29 11:32:13 +0530

    way of decaliring classes

--- Intermediate/classes.cs
+++ Intermediate/classes.cs
@@ -1,8 +1,37 @@
 
-Aman obj = new Aman();
-obj.Skills();
-Console.WriteLine(obj.age);
-Console.WriteLine(obj.name);
+
+// Newer way of calling an object..
+// Aman obj = new Aman();
+// obj.Skills();
+// Console.WriteLine(obj.age);
+// Console.WriteLine(obj.name);
+
+// public class Aman
+// {
+//     public string name = "Uniyal";
+//     public int age = 20;
+
+//     public void Skills()
+//     {
+//         Console.WriteLine($"{name} is a software Developer");
+//     }
+
+// }
+
+// Older Way
+// ALway remember to create classes in Capital letter (starting word)
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aman obj = new Aman();
+        obj.Skills();
+        Console.WriteLine(obj.age);
+        Console.WriteLine(obj.name);
+    }
+}
 public class Aman
 {
     public string name = "Uniyal";
@@ -12,5 +41,5 @@ public class Aman
     {
         Console.WriteLine($"{name} is a software Developer");
     }
-
 }
+
