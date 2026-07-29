fa0a1ca4f640fdd63a83b555925ebe78bcbd1c33
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-29 07:46:16 +0530

    Fibonacci number practice

--- Basics/list.cs
+++ Basics/list.cs
@@ -4,6 +4,8 @@
 // };
 
 
+using System.ComponentModel;
+
 List<string> animals = ["Lion", "Elephant", "Dog"];
 foreach (var animal in animals)
 {
@@ -15,4 +17,15 @@ animals.Remove("Dog");
 foreach (var name in animals)
 {
     Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+//  FIBONACCI
+List<int> fibonacci = [1, 1];
+var previous = fibonacci[fibonacci.Count - 1];
+var previous2 = fibonacci[fibonacci.Count - 2];
+fibonacci.Add(previous + previous2);
+
+foreach (var item in fibonacci)
+{
+    Console.WriteLine(item);
 }
