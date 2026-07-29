9ec808707a349f2b925bf7ee23faa9386fb07411
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-29 07:34:35 +0530

    Modifications in List

--- Basics/list.cs
+++ Basics/list.cs
@@ -1,9 +1,18 @@
-var animals = new List<string>
-{
-    "Lion","Elephant","Dog","Cat"
-};
+// var animals = new List<string>
+// {
+//     "Lion","Elephant","Dog","Cat"
+// };
 
+
+List<string> animals = ["Lion", "Elephant", "Dog"];
 foreach (var animal in animals)
 {
     Console.WriteLine($"{animal.ToUpper()}");
+}
+animals.Add("Maria");
+animals.Add("Bill");
+animals.Remove("Dog");
+foreach (var name in animals)
+{
+    Console.WriteLine($"Hello {name.ToUpper()}!");
 }
