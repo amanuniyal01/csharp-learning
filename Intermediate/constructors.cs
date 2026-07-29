a7f1caf8329c8fefc1db969f4267f638d1ea94c8
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-29 14:03:44 +0530

    Basics of constructors

---
+++ Intermediate/constructors.cs
@@ -0,0 +1,5 @@
+// A constructor is a special method that is used to initialize objects.
+// Name of a constructor method is same name as class. like if class Name is Car so constructor will be of Car name.
+// It doesn't return any value..
+//  Every class should have a constructor if we are making instance of it. Let's say if we don't make constructor in class so by default
+// an implicit constructor gets defined in the class by the compiler..
