4ffe2de9869e05519df2cb65576cc6f5a6b06666
amanuniyal01 <amanuniyal315@gmail.com>
2026-07-29 16:21:00 +0530

    ways to use interface && why to use interfaces.

--- Intermediate/Interface.cs
+++ Intermediate/Interface.cs
@@ -1,5 +1,6 @@
 // An interface is a contract. It says "any class that implements me must provide these methods" — but the interface
 //  itself provides no actual code, just the promise/signature.
+// conventionally starts with I.
 
 
 using System;
@@ -23,4 +24,20 @@ public class Dog : IAnimal
     {
         Console.WriteLine("Woof");
     }
+}
+
+
+// Interface
+interface ILogger
+{
+    void Log(string message);
+    string Name { get; }
+}
+
+public class ConsoleLogger : ILogger
+{
+    public string Name => "Console";
+    public void Log(string message) =>
+        Console.WriteLine($"[{Name}] {message}");
+
 }
